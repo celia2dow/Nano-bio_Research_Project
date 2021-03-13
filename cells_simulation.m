e5e1269a675232f3a226d45df2889eb056ef84ae
Celia Dowling <79957493+celia2dow@users.noreply.github.com>
2021-03-13 16:57:53 +1100

    Proliferation cell cycle included + better movie
    Note that the multi-phase cell cycle implemented relies on the input of K probabilities of moving from one phase to another. I will work on turning this into a queue of K servers with exponential waiting times of given rates.

--- cells_simulation.m
+++ cells_simulation.m
@@ -1,5 +1,5 @@
 function evolution_info = cells_simulation(total_t, N_initial, P_m, ...
-    P_p, DIM, speed)
+    cycle_probs, DIM, speed)
 % cells_simulation simulates the random proliferation and movement of cells
 % in a 2.D cell monolayer within a culture dish.
 %
@@ -12,7 +12,10 @@ function evolution_info = cells_simulation(total_t, N_initial, P_m, ...
 %       total_t     total number of time steps
 %       N_initial   initial number of agents (cells) in the lattice
 %       P_m         ability of an agent (cell) to move during each timestep
-%       P_p         " " " to proliferate " " "
+%       cycle_probs a list of K probabilities of transitioning between
+%                   stages in the cell cycle (e.g. stage 1 to stage 2, 
+%                   stage 2 to stage 3, ..., stage N-1 to stage N, stage N
+%                   to stage 1 having proliferated)
 %       DIM         lattice dimensions (DIM by DIM)
 %       speed       speed of movie frame playback (how many frames per sec)
 %
@@ -32,28 +35,47 @@ rng(22)
 
 % Initialise
 t=0;
-N_t = N_initial;
-cell_pop = [N_initial zeros(1,total_t)];
-cell_lin = zeros(total_t,3);
+N_t = N_initial; % the number of cells at timestep t
+K = length(cycle_probs); % the number of phases in the cell cycle
+cell_pop = [N_t zeros(1,total_t)]; % array for cell_population field
+cell_lin = zeros(total_t,3); % array for cell_lineage field
 
-% Randomise position of cells on lattice
+% Randomise position of cells on lattice and their phases
 culture_dish = zeros(DIM);
 rand_lattice_sites = randperm(numel(culture_dish));
-cell_sites = zeros(1, numel(culture_dish)); % preallocate space
-cell_sites(1:N_initial) = rand_lattice_sites(1:N_initial);
-culture_dish(cell_sites(1:N_initial)) = 1;
+cell_sites = zeros(1, numel(culture_dish)); % preallocate space for array
+                                            % tracking location of cells
+cell_sites(1:N_t) = rand_lattice_sites(1:N_t); % lattice sites for initial
+                                               % cells
+initial_cycle_phases = datasample(1:K, N_t); % cycle phases for initial
+                                             % cells
+culture_dish(cell_sites(1:N_t)) = initial_cycle_phases;
+
+% Record each cells' phase in an array that corresponds to the cell_sites
+% array (i.e, cell number i at position cell_sites(i) will have phase
+% cell_phases(i))
+cell_phases = zeros(1, numel(culture_dish)); % preallocate space
+culture_dish_as_list = culture_dish(1:numel(culture_dish));
+cell_phases(1:N_t) = culture_dish_as_list(cell_sites(1:N_t));
+
 
 % Prepare movie
 cell_fig = figure;
 axis tight manual % ensures getframe() returns a consistent size
+%ax = gca;
+%ax.NextPlot = 'replaceChildren';
 cell_movie(total_t + 1) = struct('cdata',[],'colormap',[]);
 cell_fig.Visible = 'off';
 
 % Save first movie frame (the initial culture dish)
-spy(culture_dish);
+[rows, cols] = ind2sub(DIM, cell_sites);
+c = [cell_phases(1:N_t)'/K zeros(N_t,2)]; % colours for phases
+scatter(cols(1:N_t), DIM - rows(1:N_t) + 1, 10*cell_phases(1:N_t), c, ...
+    'filled'); % Ensuring the graph represents the lattice positioning
+%xlim([0.5 DIM+0.5]); ylim([0.5 DIM+0.5]);
 title(sprintf('timestep = %d',t));
 drawnow
-cell_movie(t+1) = getframe;
+cell_movie(t+1) = getframe(cell_fig);
 
 % Loop stops when timesteps are up or when culture dish is full
 while t < total_t && all(culture_dish, 'all') == 0 
@@ -64,11 +86,11 @@ while t < total_t && all(culture_dish, 'all') == 0
     % N_t agents are selected with replacement, at random, one at a time
     % and are given a chance to move
     for choice = 1:N_t
-        selected_to_move = datasample(cell_sites(1:N_t),1);
         if rand <= P_m  % only a portion of those selected will try to move
+            current_site = datasample(cell_sites(1:N_t),1);
+            phase = cell_phases(cell_sites == current_site);
             
             % Convert linear indices to [row,column] coordinates 
-            current_site = selected_to_move;   
             [current_row, current_col] = ind2sub(DIM, current_site);
         
             % Each selected agent chooses a random direction to move in 
@@ -87,7 +109,7 @@ while t < total_t && all(culture_dish, 'all') == 0
             % If the new site is vacant, the cell moves
             if culture_dish(new_site) == 0
               culture_dish(current_site) = 0;
-              culture_dish(new_site) = 1;
+              culture_dish(new_site) = phase;
               cell_sites(cell_sites == current_site) = new_site;
             end
         end
@@ -98,58 +120,76 @@ while t < total_t && all(culture_dish, 'all') == 0
     % N_t agents are selected with replacement, at random, one at a time
     % and are given a chance to proliferate
     for choice = 1:N_t
-        selected_to_prolif = datasample(cell_sites(1:N_t),1);
-        if rand <= P_p  % only a portion of those selected will try to
-                        % proliferate
-                        
+        parent_site = datasample(cell_sites(1:N_t),1);
+        old_phase = cell_phases(cell_sites == parent_site);
+        
+        if rand <= cycle_probs(old_phase)
             % Convert linear indices to [row, column] coordinates
-            parent_site = selected_to_prolif;   
             [parent_row, parent_col] = ind2sub(DIM, parent_site);
-
-            % Each parent agent attempting proliferation chooses a random 
-            % direction in which to proliferate (create a daughter cell)
-            delta = randsample([randsample([-1,1],1),0],2);
-            daughter_row_col = [parent_row, parent_col] + delta;
+            if old_phase == K   % A cell at the end of it's cycle (ready to
+                                % proliferate)
+                % Each parent cell in their final cycle phase attempting to 
+                % proliferate chooses a random direction in which to 
+                % proliferate (create a daughter cell)
+                delta = randsample([randsample([-1,1],1),0],2);
+                daughter_row_col = [parent_row, parent_col] + delta;
         
-            % For every parent cell that proliferates out of the lattice on 
-            % one side, another cell is born into the lattice on the 
-            % opposite side
-            daughter_row_col(daughter_row_col > DIM) = 1;
-            daughter_row_col(daughter_row_col < 1) = DIM; 
-        
-            % Convert [row,column] coordinates to linear indices
-            daughter_site = sub2ind([DIM,DIM], daughter_row_col(1), ...
-                daughter_row_col(2));
-        
-            % If the new site is vacant, the cell proliferates in it
-            if culture_dish(daughter_site) == 0
-                culture_dish(daughter_site) = 1;
-                N_t = N_t + 1;
-                cell_sites(N_t) = daughter_site;
+                % For every parent cell that proliferates out of the 
+                % lattice on one side, another cell is born into the 
+                % lattice on the opposite side
+                daughter_row_col(daughter_row_col > DIM) = 1;
+                daughter_row_col(daughter_row_col < 1) = DIM; 
                 
-                % Add the [parent cell #, daughter cell #, generation #] to
-                % track lineage
-                parent_cell_num = find(cell_sites == parent_site);
-                parent_not_gen_1 = (cell_lin(:,2) == parent_cell_num);
-                if any(parent_not_gen_1)
-                    gen_num = cell_lin(parent_not_gen_1, 3) + 1;
+                % Convert [row,column] coordinates to linear indices
+                daughter_site = sub2ind([DIM,DIM], daughter_row_col(1), ...
+                    daughter_row_col(2));
+        
+                % If the new site is vacant, the cell proliferates in it
+                if culture_dish(daughter_site) == 0
+                    culture_dish(daughter_site) = 1;
+                    N_t = N_t + 1;
+                    cell_sites(N_t) = daughter_site;
+                    cell_phases(N_t) = 1;
+                    new_phase = 1;
+                    
+                    % Add the [parent cell #, daughter cell #, generation #]
+                    % to track lineage
+                    parent_cell_num = find(cell_sites == parent_site);
+                    parent_not_gen_1 = (cell_lin(:,2) == parent_cell_num);
+                    if any(parent_not_gen_1)
+                        gen_num = cell_lin(parent_not_gen_1, 3) + 1;
+                    else
+                        gen_num = 2;
+                    end
+                    cell_lin(N_t - N_initial,:) = [parent_cell_num, N_t, ...
+                        gen_num];
                 else
-                    gen_num = 2;
+                    new_phase = K;
                 end
-                cell_lin(N_t - N_initial,:) = [parent_cell_num, N_t, ...
-                    gen_num];
+            else
+                % If a cell is not in its final cycle phase, it transitions
+                % to the next phase.
+                new_phase = old_phase + 1;
             end
-        end        
+            
+            % Update chosen cell's phase
+            cell_phases(cell_sites == parent_site) = new_phase;
+            culture_dish(parent_site) = new_phase;
+        end
     end
     
     %%% RECORD %%%
     
     % Save each figure as a frame in the movie illustrating the evolution
     % of the culture dish
-    spy(culture_dish);
-    %title(sprintf('timestep = %d',t)); % 
+    [rows, cols] = ind2sub(DIM, cell_sites);
+    c = [cell_phases(1:N_t)'/K zeros(N_t,2)]; % colours for phases
+    scatter(cols(1:N_t), DIM - rows(1:N_t) + 1, 10*cell_phases(1:N_t), ...
+        c, 'filled'); % Ensuring the graph represents the lattice positioning
+    %xlim([0.5 DIM+0.5]); ylim([0.5 DIM+0.5]);
+    title(sprintf('timestep = %d',t));
     drawnow
-    cell_movie(t+1) = getframe;
+    cell_movie(t+1) = getframe(cell_fig);
     
     % Record the cell population at each timestep
     cell_pop(t+1) = N_t;
@@ -157,7 +197,7 @@ end
 
 % Play movie
 cell_fig.Visible = 'on';
-movie(cell_movie, 1, speed);
+movie(cell_fig, cell_movie, 1, speed);
 
 % Save evolution information into a structure
 evolution_info = struct('cell_population', cell_pop(1:t+1), ...
