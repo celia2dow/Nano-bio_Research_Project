3e9ad54b8c0586786358697979850b63efd3e4a6
Celia Dowling <79957493+celia2dow@users.noreply.github.com>
2021-04-24 18:30:17 +1000

    Fix visualisation, x from Poisson, count min func
    Add a switch between 3 types of visualisation (0=off) and add a count of the number of times the min function is used when drawing x, the number of particles a cell will interact with from a Poisson distribution.

--- cells_simulation.m
+++ cells_simulation.m
@@ -1,6 +1,6 @@
 function evolution_info = cells_simulation(total_t, N_initial, DIM, ...
-    max_prtcl, P_move, P_inherit, cycle_probs, rate_interacts, ...
-    base_prtcl_probs, speed)
+    siz_cell, max_prtcl, P_move, P_inherit, cycle_probs, ...
+    rate_interacts, base_prtcl_probs, speed, visual)
 % CELLS_SIMULATION simulates the random proliferation and movement of cells
 % in a 2.D cell monolayer within a culture dish.
 %
@@ -13,6 +13,7 @@ function evolution_info = cells_simulation(total_t, N_initial, DIM, ...
 %       total_t     total number of timesteps
 %       N_initial   initial number of cells in the lattice
 %       DIM         lattice dimensions (DIM by DIM)
+%       siz_cell    average diameter of particlular cell-type (micrometers)
 %       max_prtcl   maximum number of particles that a cell can internalise
 %       P_move      probability that a cell moves in 1 timestep
 %       P_inherit   the probability of a daughter cell born into the site
@@ -33,6 +34,10 @@ function evolution_info = cells_simulation(total_t, N_initial, DIM, ...
 %                           can be 1 probability per transition or can be 1
 %                           probability per cell phase per transition
 %       speed       speed of movie frame playback (frames per sec)
+%       visual      the form of visualisation desired which can be 0,1,2
+%                       0 = off
+%                       1 = slower, more comprehensive simulation
+%                       2 = faster, less comprehensive simulation
 %
 %   The output arguments are:
 %       
@@ -45,6 +50,9 @@ function evolution_info = cells_simulation(total_t, N_initial, DIM, ...
 %                               cells (i.e free particles), interacting
 %                               particles and internalised particles over
 %                               time
+%           count_mins  the number of times the min function was used in
+%                       drawing a number of particles to interact with
+%                       imperfectly from a Poisson distribution
 %
 %   This is the work of Celia Dowling 22/3/21
 
@@ -57,21 +65,27 @@ close all;
 rng(22)
 
 % Initialise variables and constants
-t=0; % the timestep
+t = 0; % the timestep
 N_t = N_initial; % the number of cells at timestep t
 prtcls_initial = N_t * 100; % the number of particles initially
-K = length(cycle_probs); % the number of phases in the cell proliferation cycle
-[check,L] = size(base_prtcl_probs); % "" in the cell-particle interaction model
+total_sites = DIM * DIM; % total number of possible positions in petri dish
+
+% Check how many stages in the cell-particle interactions model there are
+% and whether or not the user has defined different probabilities for
+% different cell-proliferation phases (matrix input) or not (vector input)
+[check,L] = size(base_prtcl_probs); 
 swtch = 0;
 if check > 1
     swtch = 1;
 end
-total_sites = DIM * DIM; % total number of possible positions in petri dish
+% The number of phases in the cell proliferation cycle
+K = length(cycle_probs); 
 
-% Initialise arrays for evolution_info fields
+% Initialise arrays/constants for evolution_info fields
 population = [N_t zeros(1,total_t)]; % cell_population
 lineage = [zeros(N_t,1) (1:N_t)' ones(N_t,1); zeros(total_t,3)]; % cell_lineage
-% Records free particles, interacting particles and internalised particles
+count_mins = 0; % number of times the min function has been used
+% Record free particles, interacting particles and internalised particles
 % per timestep in system
 tally_prtcls = [prtcls_initial zeros(1, total_t); zeros(2, total_t + 1)];
 
@@ -92,22 +106,26 @@ cell_phases(1:N_t) = datasample(1:K, N_t);
 % to simulate the number of particles hovering around one cell initially.
 cell_prtcls = zeros(total_sites, L+1);
 
-% Prepare movie
-petri_fig = figure;
-axis tight manual % ensures getframe() returns a consistent size
-%ax = gca;
-%ax.NextPlot = 'replaceChildren';
-petri_movie(total_t + 1) = struct('cdata',[],'colormap',[]);
-petri_fig.Visible = 'off';
-
-% Save first movie frame (the initial culture dish)
-draw_frame(N_t, DIM, t, L, max_prtcl, cell_sites, cell_phases, cell_prtcls)
-petri_movie(t+1) = getframe(petri_fig);
+if visual
+    % Prepare movie
+    petri_fig = figure;
+    axis tight manual % ensures getframe() returns a consistent size
+    %ax = gca;
+    %ax.NextPlot = 'replaceChildren';
+    petri_movie(total_t + 1) = struct('cdata',[],'colormap',[]);
+    petri_fig.Visible = 'off';
+    
+    % Save first movie frame (the initial culture dish)
+    draw_frame(N_t, DIM, t, L, max_prtcl, cell_sites, cell_phases, ...
+        cell_prtcls, visual, siz_cell)
+    petri_movie(t+1) = getframe(petri_fig);
+end
 
 % Loop stops when timesteps are up or when culture dish is full
-while t < total_t && ~all(culture_dish, 'all') 
+while t < total_t && ~all(tally_prtcls(:,t+1) == [0; ...
+        prtcls_initial-min(total_sites*max_prtcl,prtcls_initial); ...
+        min(total_sites*max_prtcl,prtcls_initial)],'all')
     t = t+1;
-    clf
     %%% CELL-PARTICLE INTERACTIONS %%%
     
     % Assuming that particles are constantly evenly dispersing themselves  
@@ -135,7 +153,7 @@ while t < total_t && ~all(culture_dish, 'all')
     num_available = sum(cell_prtcls(1:N_t,1:L),2);
     
     % Allow each cell to attempt to interact with x particles where x is
-    % drawn from a Poisson distribution with a parameter proportionate to
+    % drawn from a Poisson distribution with a parameter proportional to 
     % the number of particles available for interaction per cell
     %       lambda = rate_interacts * prtcls_avail
     cell_num_attempts = poissrnd(rate_interacts .* num_available);
@@ -147,6 +165,9 @@ while t < total_t && ~all(culture_dish, 'all')
             % bound to or hovering over the cell and record their stages.
             %x = cell_num_attempts(cell); %This is sometimes > num_avail
             x = min(num_available(cell),cell_num_attempts(cell));
+            if x == num_available(cell)
+                count_mins = count_mins + 1;
+            end
             prtcls_per_stage = cell_prtcls(cell,:);
             attempts = randsample(1:num_available(cell), x);
             prev_index = 0;
@@ -312,10 +333,14 @@ while t < total_t && ~all(culture_dish, 'all')
     
     %%% RECORD %%%
     
-    % Save each figure as a frame in the movie illustrating the evolution
-    % of the culture dish
-    draw_frame(N_t, DIM, t, L, max_prtcl, cell_sites, cell_phases, cell_prtcls)
-    petri_movie(t+1) = getframe(petri_fig);
+    if visual
+        clf
+        % Save each figure as a frame in the movie illustrating the evolution
+        % of the culture dish
+        draw_frame(N_t, DIM, t, L, max_prtcl, cell_sites, cell_phases, ...
+            cell_prtcls,visual, siz_cell)
+        petri_movie(t+1) = getframe(petri_fig);
+    end
     
     % Record the cell population at each timestep
     population(t+1) = N_t;
@@ -324,14 +349,17 @@ end
 % Print cell particles
 %disp(cell_prtcls((1:N_t),:));
 
-% Play movie
-petri_fig.Visible = 'on';
-movie(petri_fig, petri_movie, 1, speed);
+if visual
+    % Play movie
+    petri_fig.Visible = 'on';
+    movie(petri_fig, petri_movie, 1, speed);
+end
 
 % Save evolution information into a structure
 evolution_info = struct('cell_population', population(1:t+1), ...
     'cell_lineage', lineage(1:N_t,:), ...
-    'class_of_particles', tally_prtcls(:,1:t+1));
+    'class_of_particles', tally_prtcls(:,1:t+1), ...
+    'min_functions_used', count_mins);
 end
 
 
@@ -359,38 +387,62 @@ new_site = sub2ind([DIM,DIM], new_row_col(1), new_row_col(2));
 end
 
 
-function draw_frame(N_t, DIM, t, L, max_prtcl, cell_sites, cell_phases, cell_prtcls)
+function draw_frame(N_t, DIM, t, L, max_prtcl, cell_sites, cell_phases, ...
+    cell_prtcls, visual, siz_cell)
 % DRAW_FRAME Save each figure as a frame in the movie illustrating the 
 % evolution of the culture dish as cells move, proliferate and internalise
-% particles
+% particles.
 
-% Draw a scatter plot of the culture dish with:
-%       size of cell indicating phase of cell proliferation cycle
-%       opaqueness of cell colour indicating number of internalised particles
-%       width of cell outline indicating number of interacting particles
 [rows, cols] = ind2sub(DIM, cell_sites);
-% Sizes of cell depend on cell phase
-siz_cell = 10*cell_phases; 
-col_cell = [1 0 0];
-% Transparency of cell colour depends on number of internalised particles
-transp_cell = cell_prtcls(:,end)/max_prtcl;
-for cell = 1:N_t
-    % Width of edge of cell depends on number of interacting particles
-    if L == 1 || sum(cell_prtcls(cell,2:end - 1)) == 0
-        wid_edge = 0.1;
-        col_edge = [0 1 0];
-    else
-        wid_edge = 0.1 * sum(cell_prtcls(cell,2:end - 1),2);
-        col_edge = [0 0 1];
-    end 
-    % Plot point for cell ensuring the graph represents the lattice 
-    % positioning
-    scatter(cols(cell), DIM - rows(cell) + 1, siz_cell(cell), ...
-    'MarkerEdgeColor', col_edge, 'MarkerFaceColor', col_cell, ...
-    'MarkerFaceAlpha', transp_cell(cell), 'LineWidth', wid_edge);
-    hold on
+rows_um = rows * siz_cell;
+cols_um = cols * siz_cell;
+dim_um = DIM * siz_cell;
+siz_phase = 10*cell_phases; % Sizes of cell depend on cell phase
+
+if visual == 1 
+    %%% Slower, more comprehensive visual %%%
+    
+    % Draw a scatter plot of the culture dish with:
+    %       size of cell indicating phase of cell proliferation cycle
+    %       opacity of cell colour indicating number of internalised particles
+    %       width of cell outline indicating number of interacting particles
+    col_cell = [1 0 0];
+    % Transparency of cell colour depends on number of internalised particles
+    transp_cell = cell_prtcls(:,end)/max_prtcl;
+    for cell = 1:N_t
+        % Width of edge of cell depends on number of interacting particles
+        if L == 1 || sum(cell_prtcls(cell,2:end - 1)) == 0
+            wid_edge = 0.1;
+            col_edge = [0 1 0];
+        else
+            wid_edge = 0.1 * sum(cell_prtcls(cell,2:end - 1),2);
+            col_edge = [0 0 1];
+        end 
+        % Plot point for cell ensuring the graph represents the lattice 
+        % positioning
+        scatter(cols_um(cell), dim_um - rows_um(cell) + 1, siz_phase(cell), ...
+        'MarkerEdgeColor', col_edge, 'MarkerFaceColor', col_cell, ...
+        'MarkerFaceAlpha', transp_cell(cell), 'LineWidth', wid_edge);
+        hold on
+    end
+else
+    %%% Faster, less comprehensive visual %%%
+    
+    % Draw a scatter plot of the culture dish with:
+    %       size of cell indicating phase of cell proliferation cycle
+    %       pink opacity of cell colour indicating number of internalised particles
+    %       acqua opacity of cell colour indicating number of interacting particles
+    c = [1-sum(cell_prtcls(1:N_t,2:end - 1),2)./100 ... % interacting particles
+        1-(cell_prtcls(1:N_t,end)./max_prtcl) ...% particles internalised
+        ones(N_t,1)]; % to ensure that no particles gives white
+    scatter(cols_um(1:N_t), dim_um - rows_um(1:N_t) + 1, ...
+        siz_phase(1:N_t), c, 'filled', 'MarkerEdgeColor', 'k'); 
 end
-xlim([0.5 DIM+0.5]); ylim([0.5 DIM+0.5]);
+
+xlim([0.5*siz_cell dim_um+0.5*siz_cell]); 
+ylim([0.5*siz_cell dim_um+0.5*siz_cell]);
+xlabel('$x (\mu m)$', 'Interpreter', 'latex');
+ylabel('$y (\mu m)$', 'Interpreter', 'latex');
 title(sprintf('timestep = %d',t));
 drawnow
 end
