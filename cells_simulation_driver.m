3ab009c3d3bcfe795dd83fad60da38b73123b2ef
Celia Dowling <79957493+celia2dow@users.noreply.github.com>
2021-06-03 21:02:00 +1000

    Added Plot of Final Density of Particles in States

--- cells_simulation_driver.m
+++ cells_simulation_driver.m
@@ -14,25 +14,25 @@ max_prtcl = 30; % the maximum number of particles a cell can internalise
 P_move = 0.9;   % probability that an agent (cell) moves in 1 timestep
 P_inherit = 0.7; % the probability of a daughter cell born into the site of
                 % the parent cell inheriting 1 particle from the parent cell
-cycle_probs = [0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278,0.278];
+cycle_probs = [0.04,0.04,0.04];
                 % a list of K probabilities of transitioning between phases
                 % phases in the cell proliferation cycle in 1 timestep 
                 % (e.g. phase 1 to phase 2, phase 2 to phase 3, ..., phase 
                 % N-1 to phase N, phase N to phase 1 having proliferated)
                 %    note that these probabilities are discrete 
                 %    approximates of the exponential waiting time rates
-rate_interacts = 0.83;   % the binomial rate at which particles interact 
+rate_interacts = 0.002;   % the binomial rate at which particles interact 
                         % with a given cell, reflecting the dispersion of
                         % the particle type (number of particles per 
                         % timestep)
-base_prtcl_probs = [0.83,0.83,0.83]; %0.1,0.1,0.1;0.3,0.3,0.3];  
+base_prtcl_probs = [0.002,0.002,0.002]; %0.1,0.1,0.1;0.3,0.3,0.3];  
                 % a list of L base probabilities of particles transitioning
                 % between stages of the cell-particle interaction model in 
                 % 1 timestep - can be 1 probability per transition or can
                 % be 1 probability per cell phase per transition - columns
                 % inicate interaction stage and rows indicate cell phase
 speed = 2;      % speed of movie frame playback (how many frames per sec)
-visual = 2;     % the form of visualisation desired which can be 0,1,2
+visual = 0;     % the form of visualisation desired which can be 0,1,2
                 %     0 = off
                 %     1 = slower, more comprehensive simulation
                 %     2 = faster, less comprehensive simulation
@@ -58,6 +58,8 @@ fprintf("\nInteracting particles per cell (row) per hour (column): \n")
 %disp(evolution_info.cell_c_o_p(:,:,2))
 fprintf("\nInternalised particles per cell (row) per hour (column): \n")
 %disp(evolution_info.cell_c_o_p(:,:,3))
+fprintf("\nThe number of times the binomial distribution overdraws particles to internalise: \n")
+disp(evolution_info.count_catch)
 
 % Plot all of the interacting particles over time
 figure(1)
@@ -80,7 +82,7 @@ tsteps_per_hour = floor(total_tsteps/24); % number of timesteps that
 tstep = (time_plot - 1) * tsteps_per_hour + 1; % equivalent timestep
 histogram(evolution_info.cell_c_o_p(1:evolution_info.cell_population(tstep),time_plot,3))
 title(['At ' num2str(time_plot-1) ' hour/s'])
-xlabel('number interacting')
+xlabel('number internalised')
 ylabel('frequency')
 end
 sgtitle('Internalised particles over time')
@@ -92,5 +94,15 @@ histogram(evolution_info.cell_lineage(:,3),oldest_cell_gen);
 title('Population of cell generations')
 xlabel('gen')
 ylabel('frequency')
+
+% Plot the density of particles in different stages of the particle-cell
+% model
+figure(4)
+[~,L] = size(base_prtcl_probs);
+num_in_stage = zeros(1,L+1);
+for s = 1:L+1
+    num_in_stage(s) = sum(evolution_info.cell_prtcl_last(:,s),1);
+end
+histogram('BinEdges',-0.5:1:L+0.5,'BinCounts',num_in_stage)
 end
 
