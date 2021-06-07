a8b0db96ef60ce856b17f36ae3c5bdffc09afc64
Celia Dowling <79957493+celia2dow@users.noreply.github.com>
2021-06-08 01:12:18 +1000

    EWT input and carrying capacity changeable

--- cells_simulation_driver.m
+++ cells_simulation_driver.m
@@ -1,60 +1,91 @@
 % CELLS_SIMULATION_DRIVER is a script that drives the cells_simulation 
 % simulation with the specified parameters.
 %
-%   This is the work of Celia Dowling 3/6/21
+%   This is the work of Celia Dowling 8/6/21
 %
 %   The input argument for cells_simulation.m is a structure PARAMETERS 
 %   which has the following fields:
 %
-%       total_tsteps    total number of timesteps
-%       N_initial   initial number of cells in the lattice
-%       dim         lattice dimensions (dim by dim)
-%       siz_cell    average diameter of particlular cell-type (micrometers)
-%       max_prtcl   maximum number of particles that a cell can internalise
-%       P_move      probability that a cell moves in 1 timestep
-%       P_inherit   the probability of a daughter cell born into the site
-%                   of the parent cell inheriting 1 particle from the
-%                   parent cell
-%       cycle_probs a list of K probabilities of transitioning between
-%                   phases in the cell proliferation cycle in 1 timestep 
-%                   (e.g. phase 1 to phase 2, phase 2 to phase 3, ...,  
-%                   phase N-1 to phase N,phase N to phase 1 having 
-%                   proliferated)
-%                       note that these probabilities are discrete 
-%                       approximates of the exponential waiting time rates
-%       rate_interacts  the binomial rate at which particles interact with 
-%                       a given cell, reflecting the dispersion of the 
-%                       particle type (number of particles per timestep)
-%       base_prtcl_probs    a list of L base probabilities of particles 
-%                           transitioning between stages of the 
-%                           cell-particle interaction model in 1 timestep -
-%                           can be 1 probability per transition or can be 1
-%                           probability per cell phase per transition -
-%                           columns inicate interaction stage and rows 
-%                           indicate cell phase
-%       speed       speed of movie frame playback (frames per sec)
-%       visual      the form of visualisation desired which can be 0,1,2
-%                       0 = off
-%                       1 = slower, more comprehensive simulation
-%                       2 = faster, less comprehensive simulation
+%       simulation_duration (hours) Time length of the simulation.    
+%       tstep_duration (hours)      Recommended to be equal to the shortest
+%                                   expected waiting time (EWT), usually 
+%                                   EWT_move.
+%       initial_num_cells           Initial number of cells in the lattice.
+%       prtcls_per_cell             Number of particles added to the cell
+%                                   monolayer per cell
+%       cell_diam (micrometers)     Average diameter of particlular cell-type. 
+%       dim (cell diameters)        Lattice dimensions are dim by dim.
+%       EWT_move (hours)            Expected waiting time for one cell to 
+%                                   move 1 cell diameter. Must begreater than
+%                                   or equal to tstep_duration.
+%       EWTs_proliferate (hours)    List of K expected waiting times for 1
+%                                   cell to transition out of each phase in
+%                                   the cell proliferation cycle (e.g. mean 
+%                                   time spent in phase 1, mean time spent 
+%                                   in phase 2,..., mean time spent in phase
+%                                   K before proliferating). Elements must 
+%                                   be greater than or equal to
+%                                   tstep_duration. They should add up to
+%                                   give the expected waiting time for
+%                                   proliferation.
+%       EWFs_internalise (hours)    List of L expected waiting times for 1
+%                                   particle to transition out of each
+%                                   stage in the cell-particle interaction 
+%                                   model. Can be 1 EWT per stage (e.g. 
+%                                   mean time spent free or in stage 0 
+%                                   once having hit a cell, mean time spent 
+%                                   in stage 1, ..., mean time spent in stage 
+%                                   L-1 before transitioning into stage L i.e. 
+%                                   being internalised) or can be 1 EWT per
+%                                   stage per cell phase; columns indicating
+%                                   interaction stage and rows indicating
+%                                   cell phase. Elements must be greater than
+%                                   or equal to tstep_duration. They should
+%                                   add up, along with 1/rate_diffusivity,
+%                                   to give the expected waiting time for
+%                                   internalisation of 1 particle.
+%       prob_inherit                The probability of a daughter cell born 
+%                                   into the site of the parent cell 
+%                                   inheriting 1 particle from the parent
+%                                   cell.
+%       rate_prtcl_diffusivity (per tstep) The rate at which particles interact  
+%                                   with a given cell (fraction of free particles
+%                                   per timestep), reflecting the diffusivity 
+%                                   of the particle type.
+%       max_prtcl                   Maximum number of particles that a cell 
+%                                   can have in the specified cell-particle 
+%                                   interaction stage (i.e. carrying
+%                                   capacity).
+%       limiting_stage              The specified cell-particle interaction
+%                                   stage that limits the rate of particle
+%                                   internalisation. Must be between 1 and
+%                                   L for carrying capacity to be applied.
+%                                   Setting to 0 will turn off carrying
+%                                   capacity.
+%       vid_speed (frames per sec)  Speed of movie frame playback.
+%       visual                      The form of visualisation desired which 
+%                                   can be 0,1 or 2:
+%                                       0 = off
+%                                       1 = slower, more comprehensive vid
+%                                       2 = faster, less comprehensive vid
 %
 %   The output argument for cells_simulation.m is a structure 
 %   EVOLUTION_INFO containing the fields:
 %
-%       cell_population     a record of the cell population over time
-%       cell_lineage        a record of cell lineage 
+%       cell_population     A record of the cell population over time.
+%       cell_lineage        A record of cell lineage: 
 %                           [parent cell, daughter cell, generation]
-%       cell_phase_history  a record of the cell phases over time
-%       class_of_particles  a record of the number of particles that have
-%                           not been internalised by or bound to cells (i.e
-%                           free particles), interacting particles and
-%                           internalised particles over time
-%       average_c_o_p       a record of the average class of particles over
-%                           time
-%       cell_c_o_p          a 3D array recording the class of particles on 
+%       cell_phase_history  A record of the cell phases over time.
+%       average_c_o_p       A record of the average class of particles over 
+%                           time; the number that have not been internalised 
+%                           by or bound to cells (i.e free particles),
+%                           the number of interacting particles and that of
+%                           internalised particles.
+%       cell_c_o_p          A 3D array recording the class of particles
+%                           (free AND hit, interacting, internalised) on
 %                           a cellular basis after a particular amount of
-%                           time (e.g 1 hour, half an hour)
-%       count_catch         the number of times the catch (ensuring no more
+%                           time (e.g 1 hour, half an hour).
+%       count_catch         The number of times the catch (ensuring no more
 %                           particles are internalised than the carrying
 %                           capacity allows for) is used.
 
@@ -63,30 +94,34 @@ clear;
 close all;
 
 PARAMETERS = struct( ...
-    'total_tsteps', 144, ... 
-    'N_initial', 1000, ... 
-    'dim', 100, ...      
-    'siz_cell', 25, ...  
+    'simulation_duration',24, ... (hours)
+    'tstep_duration',1/6, ... (hours)
+    'initial_num_cells', 1000, ... 
+    'prtcls_per_cell', 100, ...
+    'cell_diam', 25, ... (micrometers)
+    'dim', 100, ... (cell diameters)x
+    'EWT_move', 1/6, ... (hours) 
+    'EWTs_proliferate',[4,4,4], ... (hours)
+    'EWTs_internalise',[5/3,5/3,5/3],... (hours)
+    'prob_inherit', 0.7, ...     
+    'rate_prtcl_diffusivity', 0.1, ...  (per timestep)
     'max_prtcl', 30, ... 
-    'P_move', 0.9, ...   
-    'P_inherit', 0.7, ... 
-    'cycle_probs',[0.04,0.04,0.04], ...
-    'rate_interacts', 0.1, ...  
-    'base_prtcl_probs', [0.1,0.1,0.1], ...
-    'speed', 2, ...      
+    'limiting_stage', 0,... 
+    'vid_speed', 2, ... (frames per sec)     
     'visual', 0 ...
     );
 
-folder_name = ['Simulation_totalTsteps' num2str(PARAMETERS.total_tsteps) ...
-    '_N0' num2str(PARAMETERS.N_initial) ...
+folder_name = ['SimulationDuration' num2str(PARAMETERS.simulation_duration) ...
+    '_tstepDuration' num2str(PARAMETERS.tstep_duration) ...
+    '_N0' num2str(PARAMETERS.initial_num_cells) ...
     '_dim' num2str(PARAMETERS.dim) ...
-    '_sizCell' num2str(PARAMETERS.siz_cell) ...
+    '_cellDiam' num2str(PARAMETERS.cell_diam) ...
     '_maxPrtcl' num2str(PARAMETERS.max_prtcl) ...
-    '_Pmove' num2str(PARAMETERS.P_move) ...
-    '_Pinherit' num2str(PARAMETERS.P_inherit) ...
-    '_cycleProbs' num2str(PARAMETERS.cycle_probs(1)) num2str(PARAMETERS.cycle_probs(end)) ...
-    '_rateInteracts' num2str(PARAMETERS.rate_interacts) ...
-    '_basePrtclProbs' num2str(PARAMETERS.base_prtcl_probs(1)) num2str(PARAMETERS.base_prtcl_probs(end)) ...
+    '_EWTmove' num2str(PARAMETERS.EWT_move) ...
+    '_Pinherit' num2str(PARAMETERS.prob_inherit) ...
+    '_EWTprolif' num2str(PARAMETERS.EWTs_proliferate(1)) num2str(PARAMETERS.EWTs_proliferate(end)) ...
+    '_rateDiffus' num2str(PARAMETERS.rate_prtcl_diffusivity) ...
+    '_EWTintern' num2str(PARAMETERS.EWTs_internalise(1)) num2str(PARAMETERS.EWTs_internalise(end)) ...
     ];
 
 if ~exist(folder_name, 'dir')
@@ -124,17 +159,16 @@ figure(2)
 interact_max = max(EVOLUTION_INFO.cell_c_o_p(:,:,2),[],'all');
 internal_max = max(EVOLUTION_INFO.cell_c_o_p(:,:,3),[],'all');
 x_max = max(interact_max,internal_max);
-local_max = zeros(1,25);
-for index = 2:25
+local_max = zeros(1,PARAMETERS.simulation_duration+1);
+for index = 2:PARAMETERS.simulation_duration+1
     [cells_interact,~] = histcounts(EVOLUTION_INFO.cell_c_o_p(:,index,2));
     [cells_internal,~] = histcounts(EVOLUTION_INFO.cell_c_o_p(:,index,3));
     local_max(index) = max([cells_interact(2:end), cells_internal(2:end)]);
 end
 y_max = max(local_max);
-for time_plot = 1:25
-    subplot(5,5,time_plot)
-    tsteps_per_hour = floor(PARAMETERS.total_tsteps/24); % number of timesteps in an hour
-    tstep = (time_plot - 1) * tsteps_per_hour + 1; % equivalent timestep index
+for time_plot = 1:PARAMETERS.simulation_duration+1
+    subplot(5,ceil(PARAMETERS.simulation_duration/5),time_plot)
+    tstep = (time_plot-1)/PARAMETERS.tstep_duration + 1; % equivalent timestep index
     % FREQUENCY OF CELLS WITH NUMS OF PARTICLES INTERACTING OVER TIME
     histogram(EVOLUTION_INFO.cell_c_o_p(1:EVOLUTION_INFO.cell_population(tstep),time_plot,2),...
         'FaceColor', [0,0,1], 'FaceAlpha', 0.2)
