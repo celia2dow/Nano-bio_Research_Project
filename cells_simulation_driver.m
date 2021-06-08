8766dd7c38088f0780a1137f1abc144df72b3629
Celia Dowling <79957493+celia2dow@users.noreply.github.com>
2021-06-08 14:43:42 +1000

    Fluorescent plots and saving figures
    I've generated hourly dot plots of the external fluorescence (stain 2) against the associated fluorescence (stain 1). The colour scheme works as follows: the more yellow a dot is, the greater the density weighting which corresponds to dots that are closer together or a high density of cells around this value in the loglog scale). More specifically, it uses the kernel smoothing function ksdensity() to find the probability density estimate of the sample in the two-column matrix of bivariate data [log(S1c),log(S2c)] at the values specified by [log(S2c),log(S1c)] (found https://au.mathworks.com/matlabcentral/fileexchange/65728-scatter-plot-colored-by-kernel-density-estimate, kernel smoother explained https://en.wikipedia.org/wiki/Kernel_smoother). S1c and S2c are merely the signals S1 and S2 where all 0 values have been set to realmin to avoid passing 0 through the log function.
    
    Lastly, the driver function saves all figures to a folder that is named according to all of the parameters defined for the simulation.

--- cells_simulation_driver.m
+++ cells_simulation_driver.m
@@ -93,6 +93,13 @@
 clear;
 close all;
 
+% Parameters relating to the fluorescent intensity dot plot
+stain1_P = 2000; % Without intensity transformations = 1
+stain1_C = 4000; % Without intensity transformations = 0
+stain2_P = 5000; % Without intensity transformations = 1
+stain2_C = 10000; % Without intensity transformations = 0
+alpha = 0.6666; % Without intensity transformations = 1
+
 PARAMETERS = struct( ...
     'simulation_duration',24, ... (hours)
     'tstep_duration',1/6, ... (hours)
@@ -111,7 +118,7 @@ PARAMETERS = struct( ...
     'visual', 0 ...
     );
 
-folder_name = ['SimulationDuration' num2str(PARAMETERS.simulation_duration) ...
+folder_name = ['Simulation_Duration' num2str(PARAMETERS.simulation_duration) ...
     '_tstepDuration' num2str(PARAMETERS.tstep_duration) ...
     '_N0' num2str(PARAMETERS.initial_num_cells) ...
     '_dim' num2str(PARAMETERS.dim) ...
@@ -129,7 +136,7 @@ if ~exist(folder_name, 'dir')
 end
 
 % Seed for efficiency purposes
-rng(22)
+%rng(22)
                 
 % Run simulation and collect data
 EVOLUTION_INFO = cells_simulation(PARAMETERS);
@@ -152,55 +159,97 @@ fprintf("\nInternalised particles per cell (row) per hour (column): \n")
 fprintf("\nThe number of times the binomial distribution overdraws particles to internalise: \n")
 %disp(EVOLUTION_INFO.count_catch)
 
-% Plot all of the interacting Vs internalised particles over time
-figure(2)
-% Find a limit for the y-axis to fix: the largest cell frequency to be
-% plotted
+% Find limits for the x and y-axis: the largest values to be plotted
 interact_max = max(EVOLUTION_INFO.cell_c_o_p(:,:,2),[],'all');
 internal_max = max(EVOLUTION_INFO.cell_c_o_p(:,:,3),[],'all');
 x_max = max(interact_max,internal_max);
-local_max = zeros(1,PARAMETERS.simulation_duration+1);
-for index = 2:PARAMETERS.simulation_duration+1
+local_max = zeros(1,floor(PARAMETERS.simulation_duration)+1);
+for index = 2:floor(PARAMETERS.simulation_duration)+1
     [cells_interact,~] = histcounts(EVOLUTION_INFO.cell_c_o_p(:,index,2));
     [cells_internal,~] = histcounts(EVOLUTION_INFO.cell_c_o_p(:,index,3));
     local_max(index) = max([cells_interact(2:end), cells_internal(2:end)]);
 end
 y_max = max(local_max);
-for time_plot = 1:PARAMETERS.simulation_duration+1
-    subplot(5,ceil(PARAMETERS.simulation_duration/5),time_plot)
+S1_max = stain1_P * (PARAMETERS.prtcls_per_cell + alpha*PARAMETERS.prtcls_per_cell) + stain1_C;
+S2_max = stain2_P * PARAMETERS.prtcls_per_cell + stain2_C;
+% Plot all of the interacting Vs internalised particles over time
+for time_plot = 1:floor(PARAMETERS.simulation_duration)+1
+    % FREQUENCY HISTOGRAMS
+    fig2 = figure(2);
+    subplot(5,ceil(floor(PARAMETERS.simulation_duration)/5),time_plot);
     tstep = (time_plot-1)/PARAMETERS.tstep_duration + 1; % equivalent timestep index
     % FREQUENCY OF CELLS WITH NUMS OF PARTICLES INTERACTING OVER TIME
     histogram(EVOLUTION_INFO.cell_c_o_p(1:EVOLUTION_INFO.cell_population(tstep),time_plot,2),...
-        'FaceColor', [0,0,1], 'FaceAlpha', 0.2)
+        'FaceColor', [0,0,1], 'FaceAlpha', 0.2);
     hold on
     % FREQUENCY OF CELLS WITH NUMS OF PARTICLES INTERNALISED OVER TIME
     histogram(EVOLUTION_INFO.cell_c_o_p(1:EVOLUTION_INFO.cell_population(tstep),time_plot,3),...
-        'FaceColor', [1,0,0], 'FaceAlpha', 0.2)
+        'FaceColor', [1,0,0], 'FaceAlpha', 0.2);
     hold off
-    ylim([0,y_max])
-    xlim([0,x_max])
-    title(['At ' num2str(time_plot-1) ' hour/s'])
-    xlabel('Number of particles')
-    ylabel('Cell frequency')
-    legend('Interacting','Internalised')
+    xlim([0,x_max]);
+    ylim([0,y_max]);
+    title(['At ' num2str(time_plot-1) ' hour/s']);
+    xlabel('Number of particles');
+    ylabel('Cell frequency');
+    legend('Interacting','Internalised');
+    
+    % FLUORESCENCE DOT PLOTS
+    fig3 = figure(3);
+    subplot(5,ceil(floor(PARAMETERS.simulation_duration)/5),time_plot);
+    tstep = (time_plot-1)/PARAMETERS.tstep_duration + 1; % equivalent timestep index
+    % PLOT EXTERNAL (STAIN 2) FLUORESCENCE AGAINST ASSOCIATED (STAIN 1)
+    % FLUORESCENCE
+    n_interact = EVOLUTION_INFO.cell_c_o_p(1:EVOLUTION_INFO.cell_population(tstep),time_plot,2);
+    n_internal = EVOLUTION_INFO.cell_c_o_p(1:EVOLUTION_INFO.cell_population(tstep),time_plot,3);
+    S1 = stain1_P .* (n_interact + alpha.*n_internal) + stain1_C; % signal from stain 1 (x-axis)
+    S2 = stain2_P .* n_interact + stain2_C; % signal from stain 2 (y-axis)
+    S1c = S1; S2c = S2; % Specifically for working out the coour densities
+    S1c(S1c==0)=realmin; % So that 0 is not passed through log
+    S2c(S2c==0)=realmin; % So that 0 is not passed through log
+    %S1c = S1c/S1_max; % To normalise so that it's between 0 and 1
+    %S2c = S2c/S2_max; % To normalise so that it's between 0 and 1
+    c = ksdensity([log(S1c),log(S2c)], [log(S2c),log(S1c)]);
+    dot_size = 3*ones(length(S1),1);
+    scatter(S1, S2, dot_size, c, 'filled');
+    set(gca, 'YScale', 'log','Xscale', 'log','XMinorTick','on','YMinorTick','on');
+    xlim([1,S1_max]);
+    ylim([1,S2_max]);
+    title(['At ' num2str(time_plot-1) ' hour/s']);
+    xlabel('Stain 1 (associated)');
+    ylabel('Stain 2 (external)');
+    cb = colorbar();
+    cb.Label.String = 'Density estimate';
 end
-sgtitle('Frequency of cells with certain numbers of interacting/internalised particles over time')
+sgtitle(fig2, 'Frequency of cells with certain numbers of interacting/internalised particles over time')
+fig2.Position = [100,100,1300,700];
+savefig(fig2, [pwd '\' folder_name '\Frequency_histograms'], 'compact')
+saveas(fig2, [pwd '\' folder_name '\Frequency_histograms'], 'png')
 
+sgtitle(fig3, 'External (stain 2) fluorescence of cells against associated (stain 1) fluorescence')
+fig3.Position = [100,100,1300,700];
+savefig(fig3, [pwd '\' folder_name '\Fluorescence_dot_plots'], 'compact')
+saveas(fig3, [pwd '\' folder_name '\Fluorescence_dot_plots'], 'png')
 
 % Plot the density of cells in different generations
-figure(3)
+fig4=figure(4);
 oldest_cell_gen = max(EVOLUTION_INFO.cell_lineage(:,3));
 histogram(EVOLUTION_INFO.cell_lineage(:,3),oldest_cell_gen);
 title('Population of cell generations')
 xlabel('Generation')
 ylabel('Frequency')
+fig4.Position = [100,100,1300,700];
+savefig(fig4, [pwd '\' folder_name '\Gen_densities'], 'compact')
+saveas(fig4, [pwd '\' folder_name '\Gen_densities'], 'png')
 
 % Plot proliferation events over time
-figure(4)
-scatter(0:(1/6):24,EVOLUTION_INFO.cell_population,4,'k','filled')
+fig5=figure(5);
+scatter(0:(1/6):24,EVOLUTION_INFO.cell_population,4,'k','filled');
 title('Cell population over time')
 xlabel('time $t$ hours', 'Interpreter', 'latex');
 ylabel('Cell population');
+fig5.Position = [100,100,1300,700];
+savefig(fig5, [pwd '\' folder_name '\Cell_popoulation'], 'compact');
+saveas(fig5, [pwd '\' folder_name '\Cell_popoulation'], 'png');
 
 % Plot associated Vs interacting+internalised cells over time
 d1 = EVOLUTION_INFO.average_c_o_p(2,:);     % Average interacting particles per cell over time
@@ -208,13 +257,13 @@ d2 = EVOLUTION_INFO.average_c_o_p(3,:);     % Average internalised particles per
 d3 = d1 + d2;                               % Histogram Sum ‘d1’+‘d2’
 binrng = 0:(1/6):24;                        % Create Bin Ranges
 
-figure(5)
+fig6=figure(6);
 subplot(1,2,1)
 bar(binrng, d3, 'r')
 hold on
 bar(binrng, d1, 'b')
 hold off
-legend('Internalised','Interacting')
+legend('Internalised','Interacting','Location','Best')
 title('Stacked histogram')
 xlabel('time $t$ hours', 'Interpreter', 'latex');
 ylabel('Average number of particles per cell');
@@ -224,10 +273,13 @@ plot(binrng,d2,'r--',binrng,d1,'b--');
 hold on
 plot(binrng,d3,'Color',[.5 0 .5])
 hold off
-legend('Internalised','Interacting','Associated (internalised or interacting)')
+legend('Internalised','Interacting','Associated (internalised or interacting)','Location','Best')
 title('Separate trends')
 xlabel('time $t$ hours', 'Interpreter', 'latex');
 
 sgtitle('Average number of particles associated per cell')
+fig6.Position = [100,100,1300,700];
+savefig(fig6, [pwd '\' folder_name '\Associated_vs_internalised'], 'compact')
+saveas(fig6, [pwd '\' folder_name '\Associated_vs_internalised'], 'png')
 
-save([pwd '\' folder_name '\variables.mat']);
+%save([pwd '\' folder_name '\variables.mat']);
